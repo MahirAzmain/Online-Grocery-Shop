2647a0c9641faa909542e71590dffcc06a1c11ed
Mahir Azmain Haque <mahirazmainhaque@Mahirs-MacBook-Air.local>
2024-11-16 00:48:11 +0600

    loginview page implemented

--- OnlineGroceriesSwiftUI/OnlineGroceriesSwiftUIApp.swift
+++ OnlineGroceriesSwiftUI/OnlineGroceriesSwiftUIApp.swift
@@ -5,20 +5,21 @@ import SwiftUI
 @main
 struct OnlineGroceriesSwiftUIApp: App {
     
-  //  @StateObject var mainVM = MainViewModel.shared
+    @StateObject var mainVM = MainViewModel.shared
     
     var body: some Scene {
         WindowGroup {
             
             NavigationView {
                 
-               // if mainVM.isUserLogin {
-                    //MainTabView()
-               // }else{
-              //  WelcomeView()
+                if mainVM.isUserLogin {
+                    //  MainTabView()
+                }else{
+                    WelcomeView()
                 }
             }
             
         }
     }
-
+    
+}
